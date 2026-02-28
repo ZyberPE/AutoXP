6f45b73b8950454dd43233f2b68867b93b56a034
ZyberPE <zyberpe@gmail.com>
2026-02-27 20:26:12 -0500

    Update Main.php

--- src/AutoXP/Main.php
+++ src/AutoXP/Main.php
@@ -9,7 +9,13 @@ use pocketmine\event\entity\EntityDeathEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\player\Player;
 use pocketmine\entity\Living;
-use pocketmine\block\BlockLegacyIds;
+use pocketmine\block\CoalOre;
+use pocketmine\block\IronOre;
+use pocketmine\block\GoldOre;
+use pocketmine\block\DiamondOre;
+use pocketmine\block\EmeraldOre;
+use pocketmine\block\LapisOre;
+use pocketmine\block\NetherQuartzOre;
 use pocketmine\world\sound\XpOrbPickupSound;
 
 class Main extends PluginBase implements Listener {
@@ -30,15 +36,14 @@ class Main extends PluginBase implements Listener {
 
         $xp = 0;
 
-        switch($block->getId()){
-            case BlockLegacyIds::COAL_ORE: $xp = $xpConfig["coal"] ?? 1; break;
-            case BlockLegacyIds::IRON_ORE: $xp = $xpConfig["iron"] ?? 2; break;
-            case BlockLegacyIds::GOLD_ORE: $xp = $xpConfig["gold"] ?? 3; break;
-            case BlockLegacyIds::DIAMOND_ORE: $xp = $xpConfig["diamond"] ?? 5; break;
-            case BlockLegacyIds::EMERALD_ORE: $xp = $xpConfig["emerald"] ?? 5; break;
-            case BlockLegacyIds::LAPIS_ORE: $xp = $xpConfig["lapis"] ?? 3; break;
-            case BlockLegacyIds::NETHER_QUARTZ_ORE: $xp = $xpConfig["nether_quartz"] ?? 2; break;
-        }
+        // Use instanceof for API 5 compatibility
+        if($block instanceof CoalOre) $xp = $xpConfig["coal"] ?? 1;
+        elseif($block instanceof IronOre) $xp = $xpConfig["iron"] ?? 2;
+        elseif($block instanceof GoldOre) $xp = $xpConfig["gold"] ?? 3;
+        elseif($block instanceof DiamondOre) $xp = $xpConfig["diamond"] ?? 5;
+        elseif($block instanceof EmeraldOre) $xp = $xpConfig["emerald"] ?? 5;
+        elseif($block instanceof LapisOre) $xp = $xpConfig["lapis"] ?? 3;
+        elseif($block instanceof NetherQuartzOre) $xp = $xpConfig["nether_quartz"] ?? 2;
 
         if($xp > 0){
             $player->addXp($xp);
@@ -57,29 +62,27 @@ class Main extends PluginBase implements Listener {
 
         $damager = $cause->getDamager();
 
-        if($entity instanceof Player){
-            // -------- Player killed by player --------
-            if($damager instanceof Player && $this->getConfig()->getNested("player-kill.enabled", true)){
-                $xp = $entity->getXp();
-                if($this->getConfig()->getNested("player-kill.transfer-all-xp", true)){
-                    $damager->addXp($xp);
-                    $this->playXpSound($damager);
-                    $entity->setXp(0);
-                }
-            } 
-            // -------- Player killed by TNT/other --------
-            else {
-                if($this->getConfig()->getNested("death-xp.drop-on-ground", true)){
-                    $entity->getWorld()->dropExperience($entity->getPosition(), $entity->getXp());
-                    $entity->setXp(0);
-                }
+        // -------- Player killed by another player --------
+        if($entity instanceof Player && $damager instanceof Player && $this->getConfig()->getNested("player-kill.enabled", true)){
+            $xp = $entity->getXp();
+            if($this->getConfig()->getNested("player-kill.transfer-all-xp", true)){
+                $damager->addXp($xp);
+                $this->playXpSound($damager);
+                $entity->setXp(0);
+            }
+        } 
+        // -------- Player killed by environment --------
+        elseif($entity instanceof Player){
+            if($this->getConfig()->getNested("death-xp.drop-on-ground", true)){
+                $entity->getWorld()->dropExperience($entity->getPosition(), $entity->getXp());
+                $entity->setXp(0);
             }
         } 
         // -------- Mob killed by player --------
-        else if($damager instanceof Player && $entity instanceof Living){
+        elseif($damager instanceof Player && $entity instanceof Living){
             $mobXpConfig = $this->getConfig()->get("mob-xp", []);
-            $mobName = $entity->getName();
-            $xp = $mobXpConfig[strtolower($mobName)] ?? 2;
+            $mobName = strtolower($entity->getName());
+            $xp = $mobXpConfig[$mobName] ?? 2;
             $damager->addXp($xp);
             $this->playXpSound($damager);
         }
