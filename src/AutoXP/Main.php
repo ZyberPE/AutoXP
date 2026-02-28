2990153062321c62d15e06d5b5603de7a433d82e
ZyberPE <zyberpe@gmail.com>
2026-02-27 20:40:35 -0500

    Update Main.php

--- src/AutoXP/Main.php
+++ src/AutoXP/Main.php
@@ -10,12 +10,12 @@ use pocketmine\event\entity\EntityDeathEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\player\Player;
 use pocketmine\world\sound\XpOrbPickupSound;
-use pocketmine\block\VanillaBlocks;
 
 class Main extends PluginBase implements Listener {
 
     private array $xpBlocks;
     private array $mobXp;
+    private array $playerXp = [];
     private array $configData;
 
     public function onEnable(): void {
@@ -30,11 +30,10 @@ class Main extends PluginBase implements Listener {
         $player = $event->getPlayer();
         $block = $event->getBlock();
         $xp = $this->xpBlocks[$block->getType()->getName()] ?? 0;
+
         if($xp > 0){
-            $player->addXp($xp);
-            if($this->configData["sound"]["enabled"] ?? false){
-                $player->getWorld()->addSound($player->getPosition(), new XpOrbPickupSound());
-            }
+            $this->addPlayerXp($player, $xp);
+            $this->playXpSound($player);
         }
     }
 
@@ -43,7 +42,7 @@ class Main extends PluginBase implements Listener {
         $lastDamage = $entity->getLastDamageCause();
         $xp = $this->mobXp[$entity::class] ?? 0;
 
-        if($entity instanceof Player) return; // Handled in PlayerDeathEvent
+        if($entity instanceof Player) return;
 
         if($xp > 0){
             $killer = null;
@@ -52,10 +51,8 @@ class Main extends PluginBase implements Listener {
             }
 
             if($killer instanceof Player){
-                $killer->addXp($xp);
-                if($this->configData["sound"]["enabled"] ?? false){
-                    $killer->getWorld()->addSound($killer->getPosition(), new XpOrbPickupSound());
-                }
+                $this->addPlayerXp($killer, $xp);
+                $this->playXpSound($killer);
             } else {
                 $entity->getWorld()->dropExperience($entity->getPosition(), $xp);
             }
@@ -64,30 +61,39 @@ class Main extends PluginBase implements Listener {
 
     public function onPlayerDeath(PlayerDeathEvent $event): void {
         $player = $event->getPlayer();
-        $cause = $player->getLastDamageCause();
-        $xp = $player->getXp();
+        $xp = $this->playerXp[$player->getUniqueId()->toString()] ?? 0;
 
+        $cause = $player->getLastDamageCause();
         if($cause instanceof EntityDamageByEntityEvent){
             $damager = $cause->getDamager();
-            if($damager instanceof Player){
-                $damager->addXp($xp);
-                $player->setXp(0);
-                if($this->configData["sound"]["enabled"] ?? false){
-                    $player->getWorld()->addSound($damager->getPosition(), new XpOrbPickupSound());
-                }
-            } else {
-                // Died by mob or TNT etc.
-                if($this->configData["death-xp"]["drop-on-ground"] ?? false){
-                    $player->getWorld()->dropExperience($player->getPosition(), $xp);
-                    $player->setXp(0);
+            if($damager instanceof Player && ($this->configData["player-kill"]["enabled"] ?? true)){
+                if($this->configData["player-kill"]["transfer-all-xp"] ?? true){
+                    $this->addPlayerXp($damager, $xp);
+                    $this->playXpSound($damager);
+                    $xp = 0;
                 }
             }
-        } else {
-            // Environmental death (lava, void, fall, suffocation)
-            if($this->configData["death-xp"]["drop-on-ground"] ?? false){
-                $player->getWorld()->dropExperience($player->getPosition(), $xp);
-                $player->setXp(0);
-            }
+        }
+
+        // Drop remaining XP on ground if any
+        if(($this->configData["death-xp"]["drop-on-ground"] ?? true) && $xp > 0){
+            $player->getWorld()->dropExperience($player->getPosition(), $xp);
+        }
+
+        // Reset player's stored XP
+        $this->playerXp[$player->getUniqueId()->toString()] = 0;
+    }
+
+    private function addPlayerXp(Player $player, int $xp): void {
+        $id = $player->getUniqueId()->toString();
+        if(!isset($this->playerXp[$id])) $this->playerXp[$id] = 0;
+        $this->playerXp[$id] += $xp;
+        $player->getXpManager()->addXp($xp);
+    }
+
+    private function playXpSound(Player $player): void {
+        if($this->configData["sound"]["enabled"] ?? true){
+            $player->getWorld()->addSound($player->getPosition(), new XpOrbPickupSound());
         }
     }
 }
